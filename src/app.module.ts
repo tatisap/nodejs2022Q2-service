a430a170856975b0d748268112dd42c189b5aff7
Tatsiana Sapun <tatiana.sapun@gmail.com>
2023-01-31 00:15:09 +0100

    feat: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ArtistModule } from './artist/artist.module';
+import { TrackModule } from './track/track.module';
+import { AlbumModule } from './album/album.module';
+import { FavModule } from './fav/fav.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ArtistModule, TrackModule, AlbumModule, FavModule],
   controllers: [AppController],
   providers: [AppService],
 })
