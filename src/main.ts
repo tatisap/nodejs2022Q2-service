5990131c3804bd1435f75f89e90b92777ff253a6
Tatsiana Sapun <tatiana.sapun@gmail.com>
2023-02-06 13:32:38 +0300

    feat: connect api doc

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
+import * as process from 'node:process';
+import * as path from 'node:path';
+import * as YAML from 'yamljs';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory, Reflector } from '@nestjs/core';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*' });
   app.useGlobalInterceptors(
     new ClassSerializerInterceptor(app.get(Reflector), {
       excludeExtraneousValues: true,
@@ -17,6 +22,11 @@ async function bootstrap() {
       forbidUnknownValues: true,
     }),
   );
+
+  const yaml: OpenAPIObject = YAML.load(
+    path.join(process.cwd(), './doc/api.yaml'),
+  );
+  SwaggerModule.setup('api', app, yaml);
 
   const configService = app.get(ConfigService);
   await app.listen(configService.get('APP_PORT'));
