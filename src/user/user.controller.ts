e6b2bcc837906c7257830ed0ccead3bcebb4209b
Tatsiana Sapun <tatiana.sapun@gmail.com>
2023-02-17 19:06:05 +0300

    feat: update user module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,8 +10,7 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
-import { User } from 'lib/entities';
-import { CreateUserDto, UpdatePasswordDto } from './user.dto';
+import { CreateUserDTO, PublicUserDTO, UpdatePasswordDTO } from './user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -19,41 +18,45 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  getAllUsers(): User[] {
-    return this.userService.getAllUsers();
+  async getAllUsers(): Promise<PublicUserDTO[]> {
+    const users = await this.userService.getAllUsers();
+    return users.map((user) => new PublicUserDTO(user));
   }
 
   @Get(':id')
-  getUser(@Param('id', ParseUUIDPipe) id: string): User {
-    const user = this.userService.getUser(id);
+  async getUser(
+    @Param('id', ParseUUIDPipe) id: string,
+  ): Promise<PublicUserDTO> {
+    const user = await this.userService.getUser(id);
     if (!user) {
       throw new NotFoundException('User not found');
     }
-    return user;
+    return new PublicUserDTO(user);
   }
 
   @Post()
-  async createUser(@Body() body: CreateUserDto): Promise<User> {
-    return this.userService.createUser(body);
+  async createUser(@Body() body: CreateUserDTO): Promise<PublicUserDTO> {
+    const user = await this.userService.createUser(body);
+    return new PublicUserDTO(user);
   }
 
   @Put(':id')
   async updatePassword(
     @Param('id', ParseUUIDPipe) id: string,
-    @Body() body: UpdatePasswordDto,
-  ): Promise<User> {
+    @Body() body: UpdatePasswordDTO,
+  ): Promise<PublicUserDTO> {
     const user = await this.userService.updatePassword(id, body);
     if (!user) {
       throw new NotFoundException('User not found');
     }
-    return user;
+    return new PublicUserDTO(user);
   }
 
   @Delete(':id')
   @HttpCode(204)
-  deleteUser(@Param('id', ParseUUIDPipe) id: string): void {
-    const user = this.userService.deleteUser(id);
-    if (!user) {
+  async deleteUser(@Param('id', ParseUUIDPipe) id: string): Promise<void> {
+    const deleteResult = await this.userService.deleteUser(id);
+    if (!deleteResult) {
       throw new NotFoundException('User not found');
     }
   }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
-import { UserRepository } from '../../lib/repositories';
 import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../lib/entities';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService, UserRepository],
+  providers: [UserService],
   exports: [UserService],
 })
 export class UserModule {}
