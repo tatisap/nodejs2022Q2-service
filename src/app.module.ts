04c2c8db3cf39df9e88cfe8be1e1d1ea384e282d
Tatsiana Sapun <tatiana.sapun@gmail.com>
2023-02-17 19:01:22 +0300

    feat: add db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,28 @@ import { ArtistModule } from './artist/artist.module';
 import { TrackModule } from './track/track.module';
 import { AlbumModule } from './album/album.module';
 import { FavModule } from './fav/fav.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { appConfig, configSchema } from './config';
+import { dbConfig } from './config/db.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) =>
+        configService.get('DATABASE'),
+      inject: [ConfigService],
+    }),
     UserModule,
     ArtistModule,
     TrackModule,
     AlbumModule,
     FavModule,
-    ConfigModule.forRoot({ load: [appConfig], validationSchema: configSchema }),
+    ConfigModule.forRoot({
+      load: [appConfig, dbConfig],
+      validationSchema: configSchema,
+    }),
   ],
 })
 export class AppModule {}
