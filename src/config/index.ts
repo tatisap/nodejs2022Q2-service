01c4b8218550e1b861acb38d92c5a5ef7ffb66d0
Tatsiana Sapun <t.sapun@pixelplex.io>
2023-02-25 21:12:33 +0100

    fix: adjust config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,4 @@
 export * from './app.config';
+export * from './db.config';
+export * from './auth.config';
 export * from './validation/schema.validation';
--- src/config/validation/schema.validation.ts
+++ src/config/validation/schema.validation.ts
@@ -7,4 +7,9 @@ export const configSchema = Joi.object().keys({
   POSTGRES_USERNAME: Joi.string().required(),
   POSTGRES_PASSWORD: Joi.string().required(),
   POSTGRES_DATABASE: Joi.string().required(),
+  JWT_SECRET_KEY: Joi.string().required(),
+  JWT_SECRET_REFRESH_KEY: Joi.string().required(),
+  TOKEN_EXPIRE_TIME: Joi.string().required(),
+  TOKEN_REFRESH_EXPIRE_TIME: Joi.string().required(),
+  CRYPT_SALT: Joi.number().integer().required(),
 });
