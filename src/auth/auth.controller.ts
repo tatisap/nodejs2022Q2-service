0036fd98846e313858c7dba6a5d43a0d69c3c3ce
Tatsiana Sapun <t.sapun@pixelplex.io>
2023-02-25 21:11:47 +0100

    feat: add auth controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { PublicUserDTO } from 'src/user/user.dto';
+import { LogInDTO, SignUpDTO, TokensDTO } from './auth.dto';
+import { AuthService } from './auth.service';
+import { AuthUserType } from './auth.type';
+import { AuthUser } from './decorators';
+import { RefreshJwtAuthGuard } from './guards';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  async signUp(@Body() body: SignUpDTO): Promise<PublicUserDTO> {
+    const user = await this.authService.signUp(body);
+    return new PublicUserDTO(user);
+  }
+
+  @Post('login')
+  async logIn(@Body() body: LogInDTO): Promise<TokensDTO> {
+    const response = await this.authService.logIn(body);
+    return new TokensDTO(response);
+  }
+
+  @UseGuards(RefreshJwtAuthGuard)
+  @Post('refresh')
+  async refresh(@AuthUser() payload: AuthUserType): Promise<TokensDTO> {
+    const response = await this.authService.getTokens(payload);
+    return new TokensDTO(response);
+  }
+}
