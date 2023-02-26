37afa4b2489cfe3d3b9204a2228d39b2e906fa5f
Tatsiana Sapun <t.sapun@pixelplex.io>
2023-02-26 22:19:22 +0100

    feat: implement logging interceptor

---
+++ src/logging/logging.interceptor.ts
@@ -0,0 +1,27 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { LoggingService } from './logging.service';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  constructor(private readonly loggingService: LoggingService) {}
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => {
+        const response: Response = context.switchToHttp().getResponse();
+        const { statusCode } = response;
+        this.loggingService.log(
+          `\nResponse: ${statusCode}\nData: ${JSON.stringify(data)}`,
+        );
+        return data;
+      }),
+    );
+  }
+}
