00ee3cb2349f5e297b203219c967001e8f434b8b
Tatsiana Sapun <t.sapun@pixelplex.io>
2023-02-26 22:20:55 +0100

    feat: connect implemented modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,23 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { LoggingModule } from './logging';
+import { AllExceptionsFilter } from './filter';
 import { AuthModule } from './auth';
 import { UserModule } from './user';
 import { ArtistModule } from './artist';
 import { TrackModule } from './track';
 import { AlbumModule } from './album';
 import { FavModule } from './fav';
-import { appConfig, dbConfig, authConfig, configSchema } from './config';
+import {
+  appConfig,
+  dbConfig,
+  authConfig,
+  loggingConfig,
+  configSchema,
+} from './config';
+import { LoggingMiddleware } from './logging/logging.middleware';
 
 @Module({
   imports: [
@@ -24,9 +34,20 @@ import { appConfig, dbConfig, authConfig, configSchema } from './config';
     AlbumModule,
     FavModule,
     ConfigModule.forRoot({
-      load: [appConfig, dbConfig, authConfig],
+      load: [appConfig, dbConfig, loggingConfig, authConfig],
       validationSchema: configSchema,
     }),
+    LoggingModule,
+  ],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,12 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { LoggingService } from './logging';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.enableCors({ origin: '*' });
+  app.useLogger(app.get(LoggingService));
   app.useGlobalInterceptors(
     new ClassSerializerInterceptor(app.get(Reflector), {
       excludeExtraneousValues: true,
